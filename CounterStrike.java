0fd7668be659795bea441840c5a4e6764ae2b8b6
Sergey Popov <popow_sv@ukr.net>
2022-10-18 17:36:41 +0300

    Homework CounterStrike

--- CounterStrike.java
+++ CounterStrike.java
@@ -1,35 +1,36 @@
 public class CounterStrike {
-public static void main(String[] args) {
-    String command1 = "Light";
-    double adamLight = 10;
-    double brianLight = 9;
-    double derekLight = 8;
-    double ericLight = 7;
-    double felixLight = 6;
-    double summaLight = adamLight + brianLight + derekLight + ericLight + felixLight;
-    double resultLight = summaLight / 5;
-    {
-        System.out.println("Среднее арифмитическое команды Light " + resultLight);
-    }
+    public static void main(String[] args) {
+        String command1 = "Light";
+        double adamLight = 10;
+        double brianLight = 9;
+        double derekLight = 8;
+        double ericLight = 7;
+        double felixLight = 6;
+        double summaLight = adamLight + brianLight + derekLight + ericLight + felixLight;
+        double resultLight = summaLight / 5;
+        {
+            System.out.println("Среднее арифмитическое команды Light " + resultLight);
+        }
 
-    String command2 = "Dark";
-    double albertDark = 15;
-    double billDark = 12;
-    double grantDark = 3;
-    double jackDark = 7;
-    double martinDark = 5;
-    double summaDark = albertDark + billDark + grantDark + jackDark + martinDark;
-    double resultDark = summaDark / 5;
-    {
-        System.out.println("Среднее арифмитическое команды Dark " + resultDark);
-    }
+        String command2 = "Dark";
+        double albertDark = 15;
+        double billDark = 12;
+        double grantDark = 3;
+        double jackDark = 7;
+        double martinDark = 5;
+        double summaDark = albertDark + billDark + grantDark + jackDark + martinDark;
+        double resultDark = summaDark / 5;
+        {
+            System.out.println("Среднее арифмитическое команды Dark " + resultDark);
+        }
 
-    if (resultLight > resultDark) {
-        System.out.println("Победа команды Light");
-    } else if (resultLight < resultDark) {
-        System.out.println("Победа команды Dark");
-    } else {System.out.println("Ничья между командами Light и Dark");
+        if (resultLight > resultDark) {
+            System.out.println("Победила команда " + command1 + " набравшая " + resultLight + "  очков");
+        } else if (resultLight < resultDark) {
+            System.out.println("Победила команда " + command2 + " набравшая " + resultDark + "  очков");
+        } else {
+            System.out.println("Ничья между командами  " + command1 + " и " + command2);
+        }
+
     }
-    System.out.println("Победила команда  " + command2 + "  набравшая   " + resultDark+ "  очков");
-}
 }
